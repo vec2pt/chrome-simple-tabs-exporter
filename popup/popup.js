dd78b7153e36fb56053e30566c9a2d277b775f35
vlmarch <68401718+vlmarch@users.noreply.github.com>
2023-11-03 00:05:59 +0100

    Add exportTabsAsMarkdown option

--- popup/popup.js
+++ popup/popup.js
@@ -5,8 +5,8 @@ async function exportTabs() {
 
   const paths = new Array();
   for (const tab of tabs) {
-    const pathname = tab.url;
-    paths.push(pathname);
+    const tabURL = tab.url;
+    paths.push(tabURL);
   }
 
   const pathString = paths.join("\n");
@@ -18,11 +18,40 @@ async function exportTabs() {
   const dateNow = now.toISOString().slice(0, 10).replaceAll("-", "");
   const timeNow = now.toISOString().slice(11, 19).replaceAll(":", "");;
 
-  const filename = `${dateNow}_${timeNow}_tabs.txt`;
+  const fileName = `${dateNow}_${timeNow}_tabs.txt`;
 
   chrome.downloads.download({
     url: url,
-    filename: filename,
+    filename: fileName,
+  });
+}
+
+async function exportTabsAsMarkdown() {
+  const tabs = await chrome.tabs.query({});
+  tabs.reload
+
+  const paths = new Array();
+  for (const tab of tabs) {
+    const tabTitle = tab.title;
+    const tabURL = tab.url;
+    const tabMd = `- [${tabTitle}](${tabURL})`;
+    paths.push(tabMd);
+  }
+
+  const pathString = paths.join("\n");
+
+  const blob = new Blob([pathString], { type: "text/md" });
+  const url = URL.createObjectURL(blob);
+
+  const now = new Date();
+  const dateNow = now.toISOString().slice(0, 10).replaceAll("-", "");
+  const timeNow = now.toISOString().slice(11, 19).replaceAll(":", "");;
+
+  const fileName = `${dateNow}_${timeNow}_tabs.md`;
+
+  chrome.downloads.download({
+    url: url,
+    filename: fileName,
   });
 }
 
@@ -47,4 +76,5 @@ function importTabs() {
 }
 
 document.getElementById('exportTabs').addEventListener('click', exportTabs, false);
+document.getElementById('exportTabsToMd').addEventListener('click', exportTabsAsMarkdown, false);
 document.getElementById('importTabs').addEventListener('change', importTabs, false);
